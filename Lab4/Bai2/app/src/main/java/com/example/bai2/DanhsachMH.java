53f29222f0785dac8d93b90e9b3e2723ba1d3f94
HP <nguyenhuutoan291103@gmail.com>
2024-09-29 11:45:46 +0700

    first commit

--- Lab4/Bai2/app/src/main/java/com/example/bai2/DanhsachMH.java
+++ Lab4/Bai2/app/src/main/java/com/example/bai2/DanhsachMH.java
@@ -2,7 +2,9 @@ package com.example.bai2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DanhsachMH extends AppCompatActivity {
+    ListView lv;
      Button btnTve, btnThoat;
+     final String dsmh[] = {"Công nghệ Phần mềm", "Web", "Di động", "Giao diện", "Mạng máy tính", "Hệ điều hành"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +31,9 @@ public class DanhsachMH extends AppCompatActivity {
     public void initControl(){
         btnTve = findViewById(R.id.btnTroVe);
         btnThoat = findViewById(R.id.btnThoat);
-
+        lv = findViewById(R.id.lv);
+        ArrayAdapter adap = new ArrayAdapter(DanhsachMH.this, android.R.layout.simple_list_item_1,dsmh);
+        lv.setAdapter(adap);
         btnTve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
